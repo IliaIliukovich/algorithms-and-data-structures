0f6a42862513b66c67fa086f8c4b99904fb691f2
Ilia_Iliukovich <Ilia_Iliukovich@epam.com>
2017-12-07 19:51:50 +0300

    Task1_4

---
+++ src/com/epam/Task1_4.java
@@ -0,0 +1,38 @@
+package com.epam;
+
+import java.util.Arrays;
+
+public class Task1_4 {
+
+    public static void main(String[] args) {
+
+        System.out.println(isSwapToPalidrom("abc"));
+        System.out.println(isSwapToPalidrom("abcba"));
+        System.out.println(isSwapToPalidrom("abcdcba"));
+        System.out.println(isSwapToPalidrom("dcbabca"));
+        System.out.println(isSwapToPalidrom("abcddcba"));
+        System.out.println(isSwapToPalidrom("dcbabcda"));
+        System.out.println(isSwapToPalidrom("abccbd"));
+        System.out.println(isSwapToPalidrom("aaabb"));
+
+    }
+
+    public static boolean isSwapToPalidrom(String string) {
+        char[] chars = string.toCharArray();
+        Arrays.sort(chars);
+
+        int singleCharCount = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if ((i == chars.length - 1) || (chars[i] != chars[i + 1])) {
+                if (++singleCharCount > 1) {
+                    return false;
+                }
+            } else {
+                i++;
+            }
+        }
+        return true;
+    }
+
+
+}
