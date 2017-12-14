4ec48adc81cfe08d7d26e73cd53364f188b9f206
Ilia_Iliukovich <Ilia_Iliukovich@epam.com>
2017-12-14 16:40:30 +0300

    Task1_4

--- src/com/epam/practicetask/Task1_4.java
+++ src/com/epam/practicetask/Task1_4.java
@@ -1,11 +1,13 @@
 package com.epam.practicetask;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Task1_4 {
 
     public static void main(String[] args) {
 
+        System.out.println("Sorting approach:");
         System.out.println(isSwapToPalidrom("abc"));
         System.out.println(isSwapToPalidrom("abcba"));
         System.out.println(isSwapToPalidrom("abcdcba"));
@@ -14,6 +16,16 @@ public class Task1_4 {
         System.out.println(isSwapToPalidrom("dcbabcda"));
         System.out.println(isSwapToPalidrom("abccbd"));
         System.out.println(isSwapToPalidrom("aaabb"));
+
+        System.out.println("HashMap approach:");
+        System.out.println(isSwapToPalidromHashMap("abc"));
+        System.out.println(isSwapToPalidromHashMap("abcba"));
+        System.out.println(isSwapToPalidromHashMap("abcdcba"));
+        System.out.println(isSwapToPalidromHashMap("dcbabca"));
+        System.out.println(isSwapToPalidromHashMap("abcddcba"));
+        System.out.println(isSwapToPalidromHashMap("dcbabcda"));
+        System.out.println(isSwapToPalidromHashMap("abccbd"));
+        System.out.println(isSwapToPalidromHashMap("aaabb"));
 
     }
 
@@ -32,6 +44,21 @@ public class Task1_4 {
             }
         }
         return true;
+    }
+
+    public static boolean isSwapToPalidromHashMap(String string) {
+        char[] chars = string.toCharArray();
+        HashMap<Character, Boolean> map = new HashMap<>();
+        int singleCharCount = 0;
+        for (char c : chars) {
+            singleCharCount++;
+            Boolean old = map.put(c, true);
+            if (old != null) {
+                singleCharCount = singleCharCount - 2;
+                map.put(c, null);
+            }
+        }
+        return singleCharCount <= 1;
     }
 
 
